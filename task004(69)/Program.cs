03da6d5048551daa62657e17ec65b64b57960683
Roman <romankos1993@gmail.com>
2022-11-10 22:47:36 +0300

    task 004(69) completed

---
+++ task004(69)/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 69: Напишите программу, которая на вход принимает два числа A и B, 
+// и возводит число А в целую степень B с помощью рекурсии.
+// A = 3; B = 5 -> 243 (3⁵)
+// A = 2; B = 3 -> 8
+
+void Main()
+{
+    Console.Clear();
+
+    Console.WriteLine("Return number: ");
+    int count = 0;
+    int number = int.Parse(Console.ReadLine()!);
+    Console.Write("Return degree: ");
+    int degree = int.Parse(Console.ReadLine()!);
+    int result = DegNumber(number, degree, count);
+    Console.Write(result);
+}
+int DegNumber(int number, int degree, int count)
+{
+    if (count == degree) return 1;
+    count++;
+    return number * DegNumber(number, degree, count);
+}
+
+Main();
