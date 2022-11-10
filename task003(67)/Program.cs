e5d9f699512138327a8fb9801b4e94a4a94b2335
Roman <romankos1993@gmail.com>
2022-11-10 22:46:00 +0300

    task 003(67) completed

---
+++ task003(67)/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр.
+// 453 -> 12
+// 45 -> 9
+
+void Main()
+{
+    Console.Clear();
+    int newNumber = 1;
+    Console.WriteLine("Return number: ");
+    int number = int.Parse(Console.ReadLine()!);
+    int result = SumDigital(number);
+    Console.Write(result);
+}
+    int SumDigital(int number)
+    {
+        if (number == 0) return 0;
+        return number % 10 + SumDigital(number / 10);
+    }
+
+Main();
