3e11ee82447d3db6e7cbe52f7d7d0481cf2e51b4
Roman <romankos1993@gmail.com>
2022-11-15 13:21:31 +0300

    task 003(67) chsnge 1

--- task003(67)/Program.cs
+++ task003(67)/Program.cs
@@ -5,16 +5,15 @@
 void Main()
 {
     Console.Clear();
-    int newNumber = 1;
     Console.WriteLine("Return number: ");
     int number = int.Parse(Console.ReadLine()!);
     int result = SumDigital(number);
     Console.Write(result);
 }
-    int SumDigital(int number)
-    {
-        if (number == 0) return 0;
-        return number % 10 + SumDigital(number / 10);
-    }
+int SumDigital(int number)
+{
+    if (number == 0) return 0;
+    return number % 10 + SumDigital(number / 10);
+}
 
 Main();
