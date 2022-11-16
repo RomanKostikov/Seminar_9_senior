020f6e251af0b46a98e841d58468c4f4c5c48546
Roman <romankos1993@gmail.com>
2022-11-16 18:23:31 +0300

    homework 001(64) conpleted

---
+++ homework001(64)/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 64: Задайте значение N. Напишите программу, 
+// которая выведет все чётные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+// N = 5 -> "4, 2"
+// N = 8 -> "8, 6, 4, 2,"
+
+void Main()
+{
+    Console.Clear();
+    Console.WriteLine("Return number N:");
+    int number = int.Parse(Console.ReadLine()!);
+    EvenNumber(number);
+}
+void EvenNumber(int n)
+{
+    if (n <= 1) return;
+    else
+    {
+        if (n % 2 == 0)
+        {
+            Console.Write($"{n}, ");
+            EvenNumber(n - 2);
+        }
+        else
+        {
+            n--;
+            Console.Write($"{n}, ");
+            EvenNumber(n - 2);
+        }
+
+    }
+}
+Main();
